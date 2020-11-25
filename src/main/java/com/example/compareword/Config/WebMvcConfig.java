b4e1db6e0e36c5bbeb898be7082542b798a2d887
sangnk <ngueynkhacsang100@gmail.com>
2020-11-25 22:42:18 +0700

    sas comit cmome

--- src/main/java/com/example/compareword/Config/WebMvcConfig.java
+++ src/main/java/com/example/compareword/Config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.example.compareword.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +18,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 //        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/").setCachePeriod(31556926);
         registry.addResourceHandler("/doc/**").addResourceLocations("file:src/main/resources/static/doc/").setCachePeriod(31556926);
 
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
