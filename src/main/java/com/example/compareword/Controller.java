7933e31a1ac02db13300db817d7888f7783765c3
sangnk <ngueynkhacsang100@gmail.com>
2020-11-25 19:14:39 +0700

    sas comit

--- src/main/java/com/example/compareword/Controller.java
+++ src/main/java/com/example/compareword/Controller.java
@@ -1,21 +1,18 @@
 package com.example.compareword;
 
 import com.aspose.words.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.HashMap;
 
 @RestController
 public class Controller {
@@ -34,10 +31,11 @@ public class Controller {
 //        DocSaveOptions saveOptions = new DocSaveOptions();
 //
 //        saveOptions.setSaveFormat(SaveFormat.DOC);
+//        if(docA.getWatermark().getType() == WatermarkType.TEXT) docA.getWatermark().remove();
+//
 //
 //// Export the document as an PCL file.
-//        docA.save("C:\\Users\\Admin\\Documents\\" + "Document.PclConversion_out.doc", saveOptions);
-//
+//        docA.save("C:\\Users\\Admin\\Documents\\" + "Document.test.doc", saveOptions);
 //        return ResponseEntity.ok("hello");
 //
 ////        return ResponseEntity.ok(docA.getRevisions().getCount() == 0 ? ("Documents are equal") : ("Documents are not equa"));
@@ -45,22 +43,47 @@ public class Controller {
     @PostMapping
     @CrossOrigin
     public ResponseEntity<?> report(
-            @RequestParam(value = "file", required = false) MultipartFile[] files) throws Exception {
-        for (MultipartFile file : files) {
-            String fileName =StringUtils.cleanPath(file.getOriginalFilename());
-            try {
-                Path path = Paths.get("src/main/resources/static/doc/" + fileName);
-                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            @RequestParam(value = "file1", required = false) MultipartFile file1,
+            @RequestParam(value = "file2", required = false) MultipartFile file2)
+            throws Exception {
+//        HashMap<String , ArrayList<String>> listString = new HashMap<>();
+//        ArrayList<String> list = new ArrayList<>();
+//
+//        for (MultipartFile file : files) {
+//            String fileName =StringUtils.cleanPath(file.getOriginalFilename());
+//            try {
+//                Path path = Paths.get("src/main/resources/static/doc/" + fileName);
+//                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+////                fileNames.add(path.toString());
+////                a += fileName + ',';
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            System.out.println(fileName);
+//            list.add(fileName);
+//        }
+//        listString.put("listString",list);
+        String fileName1 =StringUtils.cleanPath(file1.getOriginalFilename()).replace(" ","");
+        String fileName2 =StringUtils.cleanPath(file2.getOriginalFilename()).replace(" ","");
+        HashMap<String, String> listString = new HashMap<>();
+        listString.put("fileName1",fileName1);
+        listString.put("fileName2",fileName2);
+        try {
+                Path path1 = Paths.get("src/main/resources/static/doc/" + fileName1);
+                Files.copy(file1.getInputStream(), path1, StandardCopyOption.REPLACE_EXISTING);
 //                fileNames.add(path.toString());
 //                a += fileName + ',';
+                Path path2 = Paths.get("src/main/resources/static/doc/" + fileName2);
+                Files.copy(file1.getInputStream(), path2, StandardCopyOption.REPLACE_EXISTING);
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            System.out.println(fileName);
-        }
 
-        return ResponseEntity.ok("succes");
+
+
+        return ResponseEntity.ok(listString);
     }
 
 }
